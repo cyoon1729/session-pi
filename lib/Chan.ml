95842da2b951b9c2b8379e0d5adc6b58f6e43a3d
Chris Yoon <cjy2129@columbia.edu>
2023-03-17 21:45:22 -0400

    set up formatting and some cleanup

--- lib/Chan.ml
+++ lib/Chan.ml
@@ -2,36 +2,46 @@ open Core
 open Async
 open Pi
 
-let newPiChan (chanMap : globalMapType) 
-              (cName : int) 
-            : globalMapType =
-  let (r1, w1) = Pipe.create () in (* create one half-duplex pipe *)
-  let (r2, w2) = Pipe.create () in (* create another half-duplex pipe *)
+let newPiChan (chanMap : globalMapType) (cName : int) : globalMapType =
+  let r1, w1 = Pipe.create () in
+  (* create one half-duplex pipe *)
+  let r2, w2 = Pipe.create () in
+  (* create another half-duplex pipe *)
   (* each channel has two polarities; 
 		the + one corresponds to the positive mangled value
 		the - one corresponds to the negative mangled value *)
-  let newMap = List.fold
-                 [( cName, Deferred.return (PiChan (r1, w2))); (* add + end *)
-                  (-cName, Deferred.return (PiChan (r2, w1)))] (* add - end *)
-                 ~init:chanMap 
-                 ~f:(fun m (k, v) ->
-                       match Map.add m ~key:k ~data:v with
-                       | `Ok nm -> nm
-                       | `Duplicate -> m)
-  in newMap
+  let newMap =
+    List.fold
+      [ cName, Deferred.return (PiChan (r1, w2))
+      ; (* add + end *)
+        -cName, Deferred.return (PiChan (r2, w1))
+      ]
+      (* add - end *)
+      ~init:chanMap
+      ~f:(fun m (k, v) ->
+        match Map.add m ~key:k ~data:v with
+        | `Ok nm -> nm
+        | `Duplicate -> m)
+  in
+  newMap
+;;
 
-let getChanByName (chanMap : globalMapType)
-                  (pName : int) 
-                : (value Pipe.Reader.t * value Pipe.Writer.t) Deferred.t = 
+let getChanByName (chanMap : globalMapType) (pName : int)
+  : (value Pipe.Reader.t * value Pipe.Writer.t) Deferred.t
+  =
   (* get the ***deferred*** value associated with this mangled name *)
-  let v = (match Map.find chanMap pName with
-             | None -> raise (Failure("not in scope"))
-             | Some v -> v) in
+  let v =
+    match Map.find chanMap pName with
+    | None -> raise (Failure "not in scope")
+    | Some v -> v
+  in
   (* "peel off" a layer of types and put it back in the Deferred monad *)
-  v >>|
-  (fun v -> match v with
-			 | PiChan (x, y) -> (x, y)
-			 | _ -> raise(Failure("not a channel")))
+  v
+  >>| fun v ->
+  match v with
+  | PiChan (x, y) -> x, y
+  | _ -> raise (Failure "not a channel")
+;;
 
 (*
 TODO: fix this and figure out where to put it
@@ -42,19 +52,22 @@ let closePi chanMap (pName : int) : unit =
     Pipe.close_read r
 *)
 
-let sendPi (chanMap : globalMapType)
-           (pName : int) 
-           (data : 'a)
-         : unit =
-  (getChanByName chanMap pName) >>>    (* the actual value of the channel is deferred *)
-  (fun (_, w) -> Pipe.write w data >>> (* block until data fits into pipe *)
-  				fun () -> ())
+let sendPi (chanMap : globalMapType) (pName : int) (data : 'a) : unit =
+  getChanByName chanMap pName
+  >>> (* the actual value of the channel is deferred *)
+  fun (_, w) ->
+  Pipe.write w data
+  >>> (* block until data fits into pipe *)
+  fun () ->
+  ()
+;;
 
-let recvPi (chanMap : globalMapType)
-           (pName : int) 
-         : 'a Deferred.t = 
-  (getChanByName chanMap pName) >>=    (* the actual value of the channel is deferred *)
-  (fun (r, _) -> Pipe.read r >>|
-				 function
-				 | `Eof -> failwith "EOF"
-				 | `Ok data -> data)
+let recvPi (chanMap : globalMapType) (pName : int) : 'a Deferred.t =
+  getChanByName chanMap pName
+  >>= (* the actual value of the channel is deferred *)
+  fun (r, _) ->
+  Pipe.read r
+  >>| function
+  | `Eof -> failwith "EOF"
+  | `Ok data -> data
+;;
--- lib/Pi.ml
+++ lib/Pi.ml
@@ -4,7 +4,7 @@ open Async
 type name = string
 type chan = string
 
-type typ = 
+type typ =
   | LitInt
   | LitBool
   | LitChar
@@ -12,39 +12,47 @@ type typ =
   | TChan of typ
   | TChanTup of typ list
   | SessTyp
-and sessTyp = 
+
+and sessTyp =
   | End
   | Send of typ * sessTyp
   | Recv of typ * sessTyp
   | Select of (typ * sessTyp) list
   | Offer of (typ * sessTyp) list
 
-type pattern = 
+type pattern =
   | PatVar of name
   | PatTup of pattern list
   | Wildcard
 
-type expr = (* sth that can be sent via channels *)
+type expr =
+  (* sth that can be sent via channels *)
   | Str of string
   | Var of string
   | Plus of string
   | Minus of string
 
-type chanVar = (* channel with polarity *)
+type chanVar =
+  (* channel with polarity *)
   | Plus of string
   | Minus of string
-type pi = (* process *)
+
+type pi =
+  (* process *)
   | Nil
-  | Print of expr   
-  | Compose of pi * pi       (* P | Q *)
-  | Dot of pi * pi           (* P.Q *)
-  | New of chan * pi         (* (new c) P *)
-  | Send of chanVar * expr   (* c+/c-(x). P *)
+  | Print of expr
+  | Compose of pi * pi (* P | Q *)
+  | Dot of pi * pi (* P.Q *)
+  | New of chan * pi (* (new c) P *)
+  | Send of chanVar * expr (* c+/c-(x). P *)
   | Recv of chanVar * string (* c+/c-<x>. P *)
-  | Select of name * pi		 
+  | Select of name * pi
   | Offer of (name * pi) list
 
 (* the value of a var/chan in the globalMap *)
-type value = Strg of string | PiChan of value Pipe.Reader.t * value Pipe.Writer.t
+type value =
+  | Strg of string
+  | PiChan of value Pipe.Reader.t * value Pipe.Writer.t
+
 (* global map with deferred values *)
 type globalMapType = (int, value Deferred.t, Int.comparator_witness) Map.t
