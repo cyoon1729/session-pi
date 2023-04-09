1e31204c3293be54b468ca6f695e609ada001c28
andrei-coman <andrei-coman@outlook.com>
2023-04-09 14:15:03 -0400

    Subtyping for Session Types; TODO: regular types

--- lib/Stype.ml
+++ lib/Stype.ml
@@ -73,28 +73,73 @@ let tTypeSub (n : Pi.tType) (x : Pi.typeVar) (t : Pi.tType) : Pi.tType =
 
 (* p. 214 *)
 (* call as sTypeSubC [] [] t1 t2 *)
-let rec sTypeSubC (sSigma : (Pi.sType * Pi.sType) list) 
-                  (tSigma : (Pi.tType * Pi.tType) list) 
-                  (s1 : Pi.sType) (s2 : Pi.sType) : bool = 
-  match List.exists sSigma ~f:(fun (a, b) -> (Poly.(=) a s1) && (Poly.(=) b s2)) with
+let rec sTypeSubC
+  (sSigma : (Pi.sType * Pi.sType) Set.Poly.t)
+  (tSigma : (Pi.tType * Pi.tType) Set.Poly.t)
+  (s1 : Pi.sType)
+  (s2 : Pi.sType)
+  : bool
+  =
+  match Set.Poly.mem sSigma (s1, s2) with
   | true -> true (* AS-ASSUMP *)
-  | false -> 
-    (match (s1, s2) with
-     | (SEnd, SEnd) -> true (* AS-END *)
-     | (SMu (x, t), u) -> (* AS-RECL *)
-       sTypeSubC ((s1, s2) :: sSigma) tSigma (sTypeSub s1 x t) u
-     | (t, SMu (x, u)) -> (* AS-RECR *)
-       sTypeSubC ((s1, s2) :: sSigma) tSigma t (sTypeSub s2 x u)
-     | (SInput (ts, v), SInput (us, w)) -> (* AS-INS *)
-       (sTypeSubC sSigma tSigma v w) && 
-       (List.for_all2_exn ts us ~f:(tTypeSubC sSigma tSigma))
-     | (SOutput (ts, v), SOutput (us, w)) -> (* AS-OUTS *)
-       (sTypeSubC sSigma tSigma v w) && 
-       (List.for_all2_exn us ts ~f:(tTypeSubC sSigma tSigma))
-     | (SBranch labs1, SBranch labs2) ->
-       (* TODO *)
-    )
-and tTypeSubC (sSigma : (Pi.sType * Pi.sType) list) 
-              (tSigma : (Pi.tType * Pi.tType) list) 
-              (t1 : Pi.tType) (t2 : Pi.tType) : bool = 
-  ignore(sSigma, tSigma, t1, t2); true
+  | false ->
+    (match s1, s2 with
+     | SEnd, SEnd -> true (* AS-END *)
+     | SMu (x, t), u ->
+       (* AS-RECL *)
+       sTypeSubC (Set.Poly.add sSigma (s1, s2)) tSigma (sTypeSub s1 x t) u
+     | t, SMu (x, u) ->
+       (* AS-RECR *)
+       sTypeSubC (Set.Poly.add sSigma (s1, s2)) tSigma t (sTypeSub s2 x u)
+     | SInput (ts, v), SInput (us, w) ->
+       (* AS-INS *)
+       sTypeSubC sSigma tSigma v w && List.for_all2_exn ts us ~f:(tTypeSubC sSigma tSigma)
+     | SOutput (ts, v), SOutput (us, w) ->
+       (* AS-OUTS *)
+       sTypeSubC sSigma tSigma v w && List.for_all2_exn us ts ~f:(tTypeSubC sSigma tSigma)
+     | SBranch labs1, SBranch labs2 ->
+       (* AS-BRANCH *)
+       let smap1 =
+         match Map.Poly.of_alist labs1 with
+         | `Ok smap -> smap
+         | `Duplicate_key _ -> raise (Failure "error: duplicate label")
+       in
+       let smap2 =
+         match Map.Poly.of_alist labs2 with
+         | `Ok smap -> smap
+         | `Duplicate_key _ -> raise (Failure "error: duplicate label")
+       in
+       Map.Poly.for_alli smap1 ~f:(fun ~key ~data ->
+         let s = data in
+         match Map.Poly.find smap2 key with
+         | None -> false
+         | Some t -> sTypeSubC sSigma tSigma s t)
+     | SChoice labs1, SChoice labs2 ->
+       (* AS-CHOICE *)
+       let smap1 =
+         match Map.Poly.of_alist labs1 with
+         | `Ok smap -> smap
+         | `Duplicate_key _ -> raise (Failure "error: duplicate label")
+       in
+       let smap2 =
+         match Map.Poly.of_alist labs2 with
+         | `Ok smap -> smap
+         | `Duplicate_key _ -> raise (Failure "error: duplicate label")
+       in
+       Map.Poly.for_alli smap2 ~f:(fun ~key ~data ->
+         let t = data in
+         match Map.Poly.find smap1 key with
+         | None -> false
+         | Some s -> sTypeSubC sSigma tSigma s t)
+     | _, _ -> false)
+
+and tTypeSubC
+  (sSigma : (Pi.sType * Pi.sType) Set.Poly.t)
+  (tSigma : (Pi.tType * Pi.tType) Set.Poly.t)
+  (t1 : Pi.tType)
+  (t2 : Pi.tType)
+  : bool
+  =
+  ignore (sSigma, tSigma, t1, t2);
+  true
+;;
