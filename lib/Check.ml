550ac98d67ab80c6582f5fb2a44ebfeef5f11367
andrei-coman <andrei-coman@outlook.com>
2023-04-10 16:05:37 -0400

    Renamed eval to check

--- lib/Check.ml
+++ lib/Check.ml
@@ -6,7 +6,7 @@ type envName =
   | Mins of Pi.name
 
 (* p. 219 *)
-let rec eval
+let rec check
   (gamma : (envName, Pi.tType) Map.Poly.t)
   (x : envName Set.Poly.t)
   (ast : Pi.process)
@@ -21,18 +21,18 @@ let rec eval
       | _ -> false)
   | Par (p, q) ->
     (* TC-PAR *)
-    let y = eval gamma x p in
+    let y = check gamma x p in
     let gamma' = Set.Poly.fold y ~init:gamma ~f:Map.Poly.remove in
     let x' = Set.Poly.fold y ~init:x ~f:Set.Poly.remove in
-    let z = eval gamma' x' q in
+    let z = check gamma' x' q in
     Set.Poly.union y z
   | Rep p ->
     (* TC-REP *)
-    eval gamma Set.Poly.empty p
+    check gamma Set.Poly.empty p
   | New (namex, NChan ts, p) ->
     (* TC-NEW *)
     let gamma' = gamma |> Map.Poly.add_exn ~key:(Name namex) ~data:(Pi.NChan ts) in
-    eval gamma' x p
+    check gamma' x p
   | New (namex, SType s, p) ->
     (* TC-NEWS *)
     let gamma' =
@@ -41,7 +41,7 @@ let rec eval
       |> Map.Poly.add_exn ~key:(Mins namex) ~data:(Pi.SType (Stype.dual s))
     in
     let x' = Set.Poly.add (Set.Poly.add x (Plus namex)) (Mins namex) in
-    let y = eval gamma' x' p in
+    let y = check gamma' x' p in
     let xplus = Set.Poly.mem y (Plus namex) in
     let xmins = Set.Poly.mem y (Mins namex) in
     let ended = Stype.sTypeSubC Set.Poly.empty Set.Poly.empty SEnd s in
@@ -81,11 +81,11 @@ let rec eval
       with
       | Some (NChan ts), _, _ when subtype_vars ys ts ->
         (* TC-IN *)
-        eval gamma' x' p
+        check gamma' x' p
       | Some (SType (SInput (ts, s))), _, _ when subtype_vars ys ts ->
         (* TC-INS1 *)
         let gamma'' = Map.Poly.set gamma' ~key:(Name namex) ~data:(SType s) in
-        let y = eval gamma'' x' p in
+        let y = check gamma'' x' p in
         if Set.Poly.mem x (Name namex) && Set.Poly.mem y (Name namex)
         then y
         else raise (Failure "channel name not available or not used")
@@ -93,7 +93,7 @@ let rec eval
         (* TC-INS2 *)
         let gamma'' = Map.Poly.set gamma' ~key:(Plus namex) ~data:(SType s) in
         let x'' = Set.Poly.remove x (Mins namex) in
-        let y = eval gamma'' x'' p in
+        let y = check gamma'' x'' p in
         if Set.Poly.mem x (Plus namex) && Set.Poly.mem y (Plus namex)
         then y
         else raise (Failure "channel name not available or not used")
@@ -101,7 +101,7 @@ let rec eval
         (* TC-INS3 *)
         let gamma'' = Map.Poly.set gamma' ~key:(Mins namex) ~data:(SType s) in
         let x'' = Set.Poly.remove x (Plus namex) in
-        let y = eval gamma'' x'' p in
+        let y = check gamma'' x'' p in
         if Set.Poly.mem x (Mins namex) && Set.Poly.mem y (Mins namex)
         then y
         else raise (Failure "channel name not available or not used")
