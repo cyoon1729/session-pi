b83baabd3331ed38155a605f2b0e03621a427361
Chris Yoon <cjy2129@columbia.edu>
2023-04-24 01:42:05 -0400

    half-working printer & debugging sync eval

--- bin/main.ml
+++ bin/main.ml
@@ -1,10 +1,12 @@
-open Core
-open Async
-(*open SessionPi*)
-
-let main () = ()
+open SessionPi.Pi
+open SessionPi.SyncEval
+open SessionPi.Print
 
 let () =
-  main ();
-  never_returns (Scheduler.go ())
-;;
+  let p = Par(PInput("c", [("x", Int)], PEnd), POutput("c", [DataInt 1],PEnd)) in
+  let onestep = reduceOneStep [p] in 
+  let twostep = reduceOneStep onestep in
+  print_endline "result:";
+  print_endline (printContext onestep);
+  print_endline (printContext twostep)
+  
--- test/test.ml
+++ test/test.ml
@@ -1,11 +1,3 @@
-open Core
-open SessionPi
-
-(* Wrap them up in printing functions *)
-(* Iterate over the tests and print *)
-
 let () =
-  let chanMap = Map.empty (module String) in
-  let c, m = Chan.newPiChan chanMap "test" Pi.LitInt in
-  Chan.sendPi m c.port1 234
+  print_endline "hello"
 ;;
