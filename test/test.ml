38e6bb877e05ba4772fd01809647a3df9a767e11
andrei-coman <andrei-coman@outlook.com>
2023-05-07 00:19:15 -0400

    Sad attempt at moving tests from main to test/

--- test/test.ml
+++ test/test.ml
@@ -1,11 +1,47 @@
-open Core
-open SessionPi
+(*
+Can't figure out how to translate this to unit tests
 
-(* Wrap them up in printing functions *)
-(* Iterate over the tests and print *)
+open Core
+open Stdio
+open SessionPi
+open Check
+
+let dir_path_pass = "../test-checker/pass/"
+let dir_path_fail = "../test-checker/fail/"
+
+let check_test test =
+  printf "%-30s: " test;
+  let contents = In_channel.read_all test in
+  let lexbuf = Lexing.from_string contents in
+  let ast = Parser.pi Lexer.tokenize lexbuf in
+  Check.check ast
+;;
+
+let check_test_pass test =
+  try
+    check_test test;
+    printf "Ok"
+  with
+  | Failure m -> printf "\027[31mFail (%s)\n\027[0m" m
+;;
+
+let check_test_fail test =
+  try
+    check_test test;
+    printf "\027[31mFail\n\027[0m"
+  with
+  | Failure m -> printf "Ok (%s)\n" m
+;;
 
 let () =
-  let chanMap = Map.empty (module String) in
-  let c, m = Chan.newPiChan chanMap "test" Pi.LitInt in
-  Chan.sendPi m c.port1 234
+  printf "\n";
+  Sys_unix.ls_dir dir_path_pass
+  |> List.filter ~f:(fun x -> not @@ String.is_prefix ~prefix:"." x)
+  |> List.sort ~compare:String.compare
+  |> List.iter ~f:(fun test -> check_test_pass (dir_path_pass ^ test));
+  Sys_unix.ls_dir dir_path_fail
+  |> List.filter ~f:(fun x -> not @@ String.is_prefix ~prefix:"." x)
+  |> List.sort ~compare:String.compare
+  |> List.iter ~f:(fun test -> check_test_fail (dir_path_fail ^ test))
 ;;
+*)
